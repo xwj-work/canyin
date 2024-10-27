03b5e33b8d49c4032adce6ec51979937b1249203
xwj-work <3579039226@qq.com>
2024-10-27 16:51:59 +0800

    init::App组件修改

--- canyin/src/App.vue
+++ canyin/src/App.vue
@@ -7,6 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
+    <p>1. Open the terminal</p>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
